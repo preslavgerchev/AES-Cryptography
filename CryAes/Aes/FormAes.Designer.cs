1a0488405026dee5ef4ad06f8a81204f884a4f3e
Preslav <preslavgerchevmail@gmail.com>
2016-01-07 18:22:44 +0100

    Decryption and encryption

--- CryAes/Aes/FormAes.Designer.cs
+++ CryAes/Aes/FormAes.Designer.cs
@@ -33,33 +33,33 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.btnTest = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // tbPlain
             // 
-            this.tbPlain.Location = new System.Drawing.Point(68, 78);
-            this.tbPlain.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbPlain.Location = new System.Drawing.Point(51, 63);
             this.tbPlain.Name = "tbPlain";
-            this.tbPlain.Size = new System.Drawing.Size(513, 22);
+            this.tbPlain.Size = new System.Drawing.Size(386, 20);
             this.tbPlain.TabIndex = 0;
             this.tbPlain.Text = "12345689abcdefgh";
             // 
             // tbKey
             // 
-            this.tbKey.Location = new System.Drawing.Point(68, 11);
-            this.tbKey.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbKey.Location = new System.Drawing.Point(51, 9);
             this.tbKey.Name = "tbKey";
-            this.tbKey.Size = new System.Drawing.Size(513, 22);
+            this.tbKey.Size = new System.Drawing.Size(386, 20);
             this.tbKey.TabIndex = 1;
             this.tbKey.Text = "11223344556677889900aabbccddeeff";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 15);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(10, 12);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(30, 16);
+            this.label1.Size = new System.Drawing.Size(24, 13);
             this.label1.TabIndex = 8;
             this.label1.Text = "key";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -67,10 +67,9 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(13, 81);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(10, 66);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(37, 16);
+            this.label4.Size = new System.Drawing.Size(29, 13);
             this.label4.TabIndex = 11;
             this.label4.Text = "plain";
             this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -78,25 +77,54 @@
             // btnTest
             // 
             this.btnTest.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
-            this.btnTest.Location = new System.Drawing.Point(203, 119);
+            this.btnTest.Location = new System.Drawing.Point(152, 97);
+            this.btnTest.Margin = new System.Windows.Forms.Padding(2);
             this.btnTest.Name = "btnTest";
-            this.btnTest.Size = new System.Drawing.Size(137, 52);
+            this.btnTest.Size = new System.Drawing.Size(103, 42);
             this.btnTest.TabIndex = 14;
             this.btnTest.Text = "test";
             this.btnTest.UseVisualStyleBackColor = false;
             this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 180);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(443, 20);
+            this.textBox1.TabIndex = 15;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(13, 245);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(443, 20);
+            this.textBox2.TabIndex = 16;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.button1.Location = new System.Drawing.Point(334, 97);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 42);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "test";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // FormAes
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(593, 186);
+            this.ClientSize = new System.Drawing.Size(556, 328);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.btnTest);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.tbKey);
             this.Controls.Add(this.tbPlain);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FormAes";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -111,6 +139,9 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- CryAes/Aes/FormAes.cs
+++ CryAes/Aes/FormAes.cs
@@ -45,17 +45,75 @@ namespace Aes
         {
             byte[] inputKey = readHexString(tbKey.Text);
             Key key = new Key(inputKey);
-            Console.Out.WriteLine("\nkey:\n" + key);
 
             byte[] inputPlain = readAsciiString(tbPlain.Text);
+            string encryptedMsg = EncryptMessage(inputPlain, key);
+            textBox1.Text = encryptedMsg;
+        }
+        public static string EncryptMessage(byte[] inputPlain,Key k)
+        {
             State inputState = new State(inputPlain);
-            State shiftedRows = inputState.shiftRows();
-            Console.Out.WriteLine("state:\n" + inputState.ToMatrixString());
+            inputState = inputState.addRoundKey(k, 0);
+            for (int i = 1; i <10; i++)
+            {
+                inputState = inputState.subBytes();
+                inputState = inputState.shiftRows();
+                inputState = inputState.mixColumns();
+                inputState = inputState.addRoundKey(k, i);
+            }
+            inputState = inputState.subBytes();
+            inputState = inputState.shiftRows();
+            inputState = inputState.addRoundKey(k, 10);
+            Console.Out.WriteLine(inputState.ToMatrixString());
+            return inputState.ToString();
         }
 
-        public static string EncryptMessage(string input)
+        public static string DecryptMessage(byte[] inputPlain, Key k)
         {
-            return "";
+            State outputState = new State(inputPlain);
+            outputState = outputState.addRoundKey(k, 10);
+
+            for (int i = 9; i > 0; i--)
+            {
+                outputState = outputState.shiftRowsInv();
+                outputState = outputState.subBytesInv();
+                outputState = outputState.addRoundKey(k, i);
+                outputState = outputState.mixColumnsInv();
+
+            }
+            outputState = outputState.shiftRowsInv();
+            outputState = outputState.subBytesInv();
+            outputState = outputState.addRoundKey(k, 0);
+            Console.Out.WriteLine("FINAL");
+            Console.Out.WriteLine(outputState.ToMatrixString());
+            return outputState.ToString();
+        }
+
+        public string ConvertHex(String hexString)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i <= hexString.Length - 2; i += 2)
+            {
+                sb.Append(Convert.ToString(Convert.ToChar(Int32.Parse(hexString.Substring(i, 2), System.Globalization.NumberStyles.HexNumber))));
+            }
+            return sb.ToString();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string msg = textBox1.Text;
+            byte[] inputPlain = StringToByteArray(msg);
+            byte[] inputKey = readHexString(tbKey.Text);
+            Key key = new Key(inputKey);
+            string decryptedMsg = DecryptMessage(inputPlain, key);
+            string da = ConvertHex(decryptedMsg);
+            textBox2.Text = da;
+        }
+        public static byte[] StringToByteArray(string hex)
+        {
+            return Enumerable.Range(0, hex.Length)
+                             .Where(x => x % 2 == 0)
+                             .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+                             .ToArray();
         }
     }
 }
